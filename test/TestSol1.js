d1b5033451f982fc44bf3803704cf21190fadb41
ueno <ueno@nayuta.co>
2021-02-07 12:40:33 +0900

    show accounts

--- test/TestSol1.js
+++ test/TestSol1.js
@@ -2,6 +2,10 @@ const TestSol1 = artifacts.require("TestSol1");
 
 contract("test", async accounts => {
     it("test", async () => {
+        for (let lp = 0; lp < 10; lp++) {
+            console.log(`[${lp}] ${accounts[lp]}`);
+        }
+
         const instance = await TestSol1.deployed();
         assert.equal(await instance.calc(2), 246, "calc");
     });
